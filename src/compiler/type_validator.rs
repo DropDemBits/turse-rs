e1e51be93850a92fd0e9d78c90202d887619aa38
DropDemBits <r3usrlnd@gmail.com>
2020-07-10 22:55:14 -0400

    Refactor binary operand checking and check assignment values

--- src/compiler/type_validator.rs
+++ src/compiler/type_validator.rs
@@ -1,6 +1,6 @@
 //! Type validator upon the AST tree
 use crate::compiler::ast::{ASTVisitorMut, Expr, Stmt};
-use crate::compiler::block::{CodeBlock};
+use crate::compiler::block::CodeBlock;
 use crate::compiler::token::TokenType;
 use crate::compiler::types::{self, PrimitiveType, TypeRef, TypeTable};
 use crate::status_reporter::StatusReporter;
@@ -51,11 +51,13 @@ impl ASTVisitorMut<()> for TypeValidator<'_> {
 
                     *type_spec = expr.get_eval_type();
                 } else if value.is_some() {
+                    // TODO: Resolve & De-alias type refs
                     let asn_type = value.as_ref().unwrap().get_eval_type();
+                    let resolved_spec = &type_spec;
 
                     if asn_type != TypeRef::TypeError {
                         // Validate that the types are assignable
-                        if !types::is_assignable_to(type_spec, &asn_type, &self.type_table) {
+                        if !types::is_assignable_to(resolved_spec, &asn_type, &self.type_table) {
                             // Value to assign is the wrong type
                             self.reporter.report_error(
                                 &idents.last().as_ref().unwrap().token.location,
@@ -83,13 +85,36 @@ impl ASTVisitorMut<()> for TypeValidator<'_> {
             }
             Stmt::Assign {
                 var_ref,
-                op: _,
+                op,
                 value,
             } => {
                 self.visit_expr(var_ref);
                 self.visit_expr(value);
 
-                // TODO Validate that the types are assignable for the given operation
+                // TODO: Resolve & De-alias type refs
+                let ref_type = &var_ref.get_eval_type();
+                let value_type = &value.get_eval_type();
+
+                // Validate that the types are assignable for the given operation
+                if types::is_error(value_type) {
+                    // Silently drop propogated TypeErrors
+                } else if op.token_type == TokenType::Assign {
+                    if !types::is_assignable_to(ref_type, value_type, &self.type_table) {
+                        // Value to assign is the wrong type
+                        self.reporter.report_error(
+                            &op.location,
+                            format_args!("Assignment value is the wrong type"),
+                        );
+                    }
+                } else {
+                    if check_binary_operands(ref_type, &op.token_type, value_type, &self.type_table).is_err() {
+                        // Value to assign is the wrong type
+                        self.reporter.report_error(
+                            &op.location,
+                            format_args!("Assignment value is the wrong type"),
+                        );
+                    }
+                }
             }
             Stmt::ProcedureCall { proc_ref } => self.visit_expr(proc_ref),
             Stmt::Block { block } => {
@@ -135,120 +160,21 @@ impl ASTVisitorMut<()> for TypeValidator<'_> {
                     return;
                 }
 
-                // TODO: (xor, shl, shr) -> (nat, TypeError), (>, >=, <, <=, =, ~=, in, ~in, =>) -> boolean, (and, or) -> (nat, boolean)
+                match check_binary_operands(left_type, op, right_type, &self.type_table) {
+                    Ok(good_eval) => *eval_type = good_eval,
+                    Err(bad_eval) => {
+                        *eval_type = bad_eval;
 
-                match op {
-                    TokenType::Plus => {
-                        // Valid conditions:
-                        // - Both types are strings
-                        // - Both types are numerics (real, int, nat, etc)
-                        // - Both types are sets (not checked right now)
-                        // Otherwise, TypeError is produced
-                        if types::is_char_seq_type(left_type) && types::is_char_seq_type(right_type)
-                        {
-                            // String expr, concatenation
-                            *eval_type = TypeRef::Primitive(PrimitiveType::String_);
-                        } else if types::is_number_type(left_type)
-                            && types::is_number_type(right_type)
-                        {
-                            // Number expr, sum
-                            *eval_type =
-                                *types::common_type(left_type, right_type, &self.type_table)
-                                    .unwrap();
-                        } else {
-                            // error, report!
-                            self.reporter.report_error(
-                                loc, 
-                                format_args!(
-                                    "Operands of '{}' must both be scalars (int, real, or nat), strings, or compatible sets",
-                                    op
-                                ),
-                            );
-                            *eval_type = binary_default(op);
+                        match op {
+                            TokenType::Plus => 
+                                self.reporter.report_error(loc, format_args!("Operands of '{}' must both be scalars (int, real, or nat), strings, or compatible sets", op), ),
+                            TokenType::Minus | TokenType::Star => 
+                                self.reporter.report_error(loc, format_args!("Operands of '{}' must both be scalars (int, real, or nat), or compatible sets", op)),
+                            TokenType::Slash | TokenType::Div | TokenType::Mod | TokenType::Rem | TokenType::Exp => 
+                                self.reporter.report_error(loc, format_args!("Operands of '{}' must both be scalars (int, real, or nat)", op)),
+                            _ => todo!(),
                         }
                     }
-                    TokenType::Minus | TokenType::Star => {
-                        // Valid conditions:
-                        // - Both types are numerics (real, int, nat, etc)
-                        // - Both types are sets (not checked right now)
-                        // Otherwise, TypeError is produced
-                        if types::is_number_type(left_type) && types::is_number_type(right_type) {
-                            // Number expr, minus & mul
-                            *eval_type =
-                                *types::common_type(left_type, right_type, &self.type_table)
-                                    .unwrap();
-                        } else {
-                            // error, report!
-                            self.reporter.report_error(
-                                loc,
-                                format_args!(
-                                    "Operands of '{}' must both be scalars (int, real, or nat), or compatible sets",
-                                    op
-                                ),
-                            );
-                            *eval_type = binary_default(op);
-                        }
-                    }
-                    TokenType::Slash => {
-                        // Valid conditions:
-                        // - Both types are numerics (real, int, nat, etc)
-                        // Otherwise, TypeError is produced
-                        if types::is_number_type(left_type) && types::is_number_type(right_type) {
-                            // Number expr, real div
-                            *eval_type = TypeRef::Primitive(PrimitiveType::Real);
-                        } else {
-                            // error, report!
-                            self.reporter.report_error(
-                                loc,
-                                format_args!(
-                                    "Operands of '{}' must both be scalars (int, real, or nat)",
-                                    op
-                                ),
-                            );
-                            *eval_type = binary_default(op);
-                        }
-                    }
-                    TokenType::Div => {
-                        // Valid conditions:
-                        // - Both types are numerics (real, int, nat, etc)
-                        // Otherwise, TypeError is produced
-                        if types::is_number_type(left_type) && types::is_number_type(right_type) {
-                            // Number expr, int div
-                            *eval_type = TypeRef::Primitive(PrimitiveType::Int);
-                        } else {
-                            // error, report!
-                            self.reporter.report_error(
-                                loc,
-                                format_args!(
-                                    "Operands of '{}' must both be scalars (int, real, or nat)",
-                                    op
-                                ),
-                            );
-                            *eval_type = binary_default(op);
-                        }
-                    }
-                    TokenType::Mod | TokenType::Rem | TokenType::Exp => {
-                        // Valid conditions:
-                        // - Both types are numerics (real, int, nat, etc)
-                        // Otherwise, TypeError is produced
-                        if types::is_number_type(left_type) && types::is_number_type(right_type) {
-                            // Number expr, mod, rem & exp
-                            *eval_type =
-                                *types::common_type(left_type, right_type, &self.type_table)
-                                    .unwrap();
-                        } else {
-                            // error, report!
-                            self.reporter.report_error(
-                                loc,
-                                format_args!(
-                                    "Operands of '{}' must both be scalars (int, real, or nat)",
-                                    op
-                                ),
-                            );
-                            *eval_type = binary_default(op);
-                        }
-                    }
-                    _ => unreachable!(),
                 }
             }
             Expr::UnaryOp {
@@ -329,4 +255,70 @@ fn binary_default(op: &TokenType) -> TypeRef {
         | TokenType::Imply => TypeRef::Primitive(PrimitiveType::Boolean),
         _ => TypeRef::TypeError,
     }
+}
+
+fn check_binary_operands(
+    left_type: &TypeRef,
+    op: &TokenType,
+    right_type: &TypeRef,
+    type_table: &TypeTable,
+) -> Result<TypeRef, TypeRef> {
+    // TODO: (xor, shl, shr) -> (nat, TypeError), (>, >=, <, <=, =, ~=, in, ~in, =>) -> boolean, (and, or) -> (nat, boolean)
+
+    match op {
+        TokenType::Plus => {
+            // Valid conditions:
+            // - Both types are strings
+            // - Both types are numerics (real, int, nat, etc)
+            // - Both types are sets (not checked right now)
+            // Otherwise, TypeError is produced
+            if types::is_char_seq_type(left_type) && types::is_char_seq_type(right_type) {
+                // String expr, concatenation
+                return Ok(TypeRef::Primitive(PrimitiveType::String_));
+            } else if types::is_number_type(left_type) && types::is_number_type(right_type) {
+                // Number expr, sum
+                return Ok(*types::common_type(left_type, right_type, &type_table).unwrap());
+            }
+        }
+        TokenType::Minus | TokenType::Star => {
+            // Valid conditions:
+            // - Both types are numerics (real, int, nat, etc)
+            // - Both types are sets (not checked right now)
+            // Otherwise, TypeError is produced
+            if types::is_number_type(left_type) && types::is_number_type(right_type) {
+                // Number expr, minus & mul
+                return Ok(*types::common_type(left_type, right_type, &type_table).unwrap());
+            }
+        }
+        TokenType::Slash => {
+            // Valid conditions:
+            // - Both types are numerics (real, int, nat, etc)
+            // Otherwise, TypeError is produced
+            if types::is_number_type(left_type) && types::is_number_type(right_type) {
+                // Number expr, real div
+                return Ok(TypeRef::Primitive(PrimitiveType::Real));
+            }
+        }
+        TokenType::Div => {
+            // Valid conditions:
+            // - Both types are numerics (real, int, nat, etc)
+            // Otherwise, TypeError is produced
+            if types::is_number_type(left_type) && types::is_number_type(right_type) {
+                // Number expr, int div
+                return Ok(TypeRef::Primitive(PrimitiveType::Int));
+            }
+        }
+        TokenType::Mod | TokenType::Rem | TokenType::Exp => {
+            // Valid conditions:
+            // - Both types are numerics (real, int, nat, etc)
+            // Otherwise, TypeError is produced
+            if types::is_number_type(left_type) && types::is_number_type(right_type) {
+                // Number expr, mod, rem & exp
+                return Ok(*types::common_type(left_type, right_type, &type_table).unwrap());
+            }
+        }
+        _ => todo!()
+    }
+
+    Err(binary_default(op))
 }
