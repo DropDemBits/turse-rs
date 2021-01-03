932988b5747953087d35eadb5610b0e87892f960
DropDemBits <r3usrlnd@gmail.com>
2021-01-03 02:25:59 -0500

    Create AST nodes from the current CST - Not used for anything right now

--- compiler/toc_dummy/src/main.rs
+++ compiler/toc_dummy/src/main.rs
@@ -1,6 +1,7 @@
 //! Dummy bin for running the new scanner and parser
 
 use std::{env, fs};
+use toc_syntax::ast::*;
 
 fn main() {
     let path: String = env::args().nth(1).expect("Missing path to source file");
@@ -8,4 +9,24 @@ fn main() {
     let parsed = toc_parser::parse(&contents);
 
     println!("Parsed output: {}", parsed.debug_tree());
+
+    let source = Source::cast(parsed.syntax()).unwrap();
+
+    for stmt in source.stmts() {
+        let range = stmt.syntax().text_range();
+        let slice = &contents[range.start().into()..range.end().into()];
+
+        match stmt {
+            Stmt::ConstVarDecl(_) => {
+                print!("cv_decl [{}]", slice);
+            }
+            Stmt::TypeDecl(_) => {
+                print!("cv_decl [{}]", slice);
+            }
+            Stmt::BlockStmt(_) => {
+                print!("block_stmt [{}]", slice);
+            }
+            _ => {}
+        }
+    }
 }
