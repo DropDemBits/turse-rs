ff6d9b51bdd8bbd9d6ea09b56a3cf76af96cf9f6
DropDemBits <r3usrlnd@gmail.com>
2021-01-08 15:45:33 -0500

    Rework benchmarks to group things together

--- compiler/toc/benches/parser_compare.rs
+++ compiler/toc/benches/parser_compare.rs
@@ -1,5 +1,5 @@
 //! Comparing parser implementations
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use toc_frontend::parser as old_parser;
 use toc_parser as new_parser;
@@ -8,40 +8,37 @@ use toc_frontend::context::{CompileContext, SourceMap};
 
 use std::sync::Arc;
 
-macro_rules! make_bench_over {
-    ($i:ident => $src_path:literal) => {
-        fn $i(c: &mut Criterion) {
-            let mut group = c.benchmark_group($src_path);
+fn compare_parsers(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Parsers");
+    let files = &[
+        ("ui_util.tu", include_str!("source_files/ui_util.tu")),
+        ("state.t", include_str!("source_files/state.t")),
+        ("match.t", include_str!("source_files/match.t")),
+        ("main.t", include_str!("source_files/main.t")),
+    ];
 
-            let source = include_str!($src_path);
-            group.bench_with_input("old parser", source, |b, src| {
-                b.iter(|| {
-                    let scanner = toc_frontend::scanner::Scanner::scan_source(black_box(src));
+    for (name, source) in files {
+        group.bench_with_input(BenchmarkId::new("Old Parser", name), &source, |b, src| {
+            b.iter(|| {
+                let scanner = toc_frontend::scanner::Scanner::scan_source(black_box(src));
 
-                    let context = CompileContext::new(SourceMap::new());
-                    let context = Arc::new(context);
+                let context = CompileContext::new(SourceMap::new());
+                let context = Arc::new(context);
 
-                    let mut parser = old_parser::Parser::new(scanner, true, context);
-                    let _r = black_box(parser.parse());
-                })
+                let mut parser = old_parser::Parser::new(scanner, true, context);
+                let _r = black_box(parser.parse());
             });
-
-            group.bench_with_input("new parser", source, |b, src| {
-                b.iter(|| {
-                    let src = black_box(src);
-                    let _r = black_box(new_parser::parse(src));
-                })
+        });
+        group.bench_with_input(BenchmarkId::new("New Parser", name), &source, |b, src| {
+            b.iter(|| {
+                let src = black_box(src);
+                let _r = black_box(new_parser::parse(src));
             });
+        });
+    }
 
-            group.finish();
-        }
-    };
+    group.finish();
 }
 
-make_bench_over!(state_t => "source_files/state.t");
-make_bench_over!(ui_util_tu => "source_files/ui_util.tu");
-make_bench_over!(main_t => "source_files/main.t");
-make_bench_over!(match_t => "source_files/match.t");
-
-criterion_group!(benches, state_t, ui_util_tu, main_t, match_t);
+criterion_group!(benches, compare_parsers);
 criterion_main!(benches);
