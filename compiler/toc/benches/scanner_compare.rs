0009d19621721fa747e143211946fbc9c4718e56
DropDemBits <r3usrlnd@gmail.com>
2021-01-08 15:45:33 -0500

    Rework benchmarks to group things together

--- compiler/toc/benches/scanner_compare.rs
+++ compiler/toc/benches/scanner_compare.rs
@@ -1,38 +1,35 @@
 //! Comparing scanner implementations
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use toc_frontend::scanner as old_scanner;
 use toc_scanner as new_scanner;
 
-macro_rules! make_bench_over {
-    ($i:ident => $src_path:literal) => {
-        fn $i(c: &mut Criterion) {
-            let mut group = c.benchmark_group($src_path);
+fn compare_scanners(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Scanners");
+    let files = &[
+        ("ui_util.tu", include_str!("source_files/ui_util.tu")),
+        ("state.t", include_str!("source_files/state.t")),
+        ("match.t", include_str!("source_files/match.t")),
+        ("main.t", include_str!("source_files/main.t")),
+    ];
 
-            let source = include_str!($src_path);
-            group.bench_with_input("old scanner", source, |b, src| {
-                b.iter(|| {
-                    let scanner = old_scanner::Scanner::scan_source(black_box(src));
-                    let _e: Vec<toc_core::token::Token> = black_box(scanner.collect());
-                })
+    for (name, source) in files {
+        group.bench_with_input(BenchmarkId::new("Old Scanner", name), &source, |b, src| {
+            b.iter(|| {
+                let scanner = old_scanner::Scanner::scan_source(black_box(src));
+                let _e: Vec<_> = black_box(scanner.collect());
             });
-
-            group.bench_with_input("new scanner", source, |b, src| {
-                b.iter(|| {
-                    let scanner = new_scanner::Scanner::new(black_box(src));
-                    let _e: Vec<_> = black_box(scanner.collect());
-                })
+        });
+        group.bench_with_input(BenchmarkId::new("New Scanner", name), &source, |b, src| {
+            b.iter(|| {
+                let scanner = new_scanner::Scanner::new(black_box(src));
+                let _e = black_box(scanner.collect_all());
             });
+        });
+    }
 
-            group.finish();
-        }
-    };
+    group.finish();
 }
 
-make_bench_over!(state_t => "source_files/state.t");
-make_bench_over!(ui_util_tu => "source_files/ui_util.tu");
-make_bench_over!(main_t => "source_files/main.t");
-make_bench_over!(match_t => "source_files/match.t");
-
-criterion_group!(benches, state_t, ui_util_tu, main_t, match_t);
+criterion_group!(benches, compare_scanners);
 criterion_main!(benches);
