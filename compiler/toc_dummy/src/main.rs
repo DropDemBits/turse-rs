fc3127afc2bafa2cd1e29449e702bff475eb17f4
DropDemBits <r3usrlnd@gmail.com>
2021-01-10 23:56:35 -0500

    Handle non-UTF-8 files in `toc_dummy`

--- compiler/toc_dummy/src/main.rs
+++ compiler/toc_dummy/src/main.rs
@@ -1,32 +1,18 @@
 //! Dummy bin for running the new scanner and parser
 
-use std::{env, fs};
-use toc_syntax::ast::*;
+use std::{env, fs, io};
+
+fn load_contents(path: &str) -> io::Result<String> {
+    let contents = fs::read(path)?;
+    let contents = String::from_utf8_lossy(&contents).to_string();
+    Ok(contents)
+}
 
 fn main() {
     let path: String = env::args().nth(1).expect("Missing path to source file");
-    let contents = fs::read_to_string(path).expect("Unable to load file");
+    let contents = load_contents(&path).expect("Unable to load file");
     let parsed = toc_parser::parse(&contents);
 
     println!("Parsed output: {}", parsed.debug_tree());
-
-    let source = Source::cast(parsed.syntax()).unwrap();
-
-    for stmt in source.stmts() {
-        let range = stmt.syntax().text_range();
-        let slice = &contents[range.start().into()..range.end().into()];
-
-        match stmt {
-            Stmt::ConstVarDecl(_) => {
-                print!("cv_decl [{}]", slice);
-            }
-            Stmt::TypeDecl(_) => {
-                print!("cv_decl [{}]", slice);
-            }
-            Stmt::BlockStmt(_) => {
-                print!("block_stmt [{}]", slice);
-            }
-            _ => {}
-        }
-    }
+    std::process::exit(if parsed.has_errors() { -1 } else { 0 });
 }
