1e2878998637d2a2f5cad9f66da358336b19a0de
DropDemBits <r3usrlnd@gmail.com>
2021-04-16 17:22:36 -0400

    Don't include `afl` on non-Linux platforms `rust-analyzer` reports errors since `afl` uses `xdg`, which is only compilable on Linux platforms
    The dependency is gated both in the `Cargo.toml` file, as well as in
    the fuzzer's main

--- fuzzers/parser_fuzz/src/main.rs
+++ fuzzers/parser_fuzz/src/main.rs
@@ -1,10 +1,27 @@
-#[macro_use]
-extern crate afl;
+// Workaround for the `afl` crate depending on `xdg`, which isn't compilable on platforms other than linux/unix
+// Running cargo [build/run] [--release] isn't affected because this is never touched, but it still trips up
+// rust-analyzer, resulting in errors outside of this repo
+#[cfg(target_os = "linux")]
+mod inner {
+    #[macro_use]
+    extern crate afl;
+
+    pub(crate) fn do_fuzz() {
+        fuzz!(|data: &[u8]| {
+            if let Ok(s) = std::str::from_utf8(data) {
+                let _ = toc_parser::parse(s);
+            }
+        });
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+mod inner {
+    pub(crate) fn do_fuzz() {
+        panic!("fuzzing is not supported on platforms other than linux");
+    }
+}
 
 fn main() {
-    fuzz!(|data: &[u8]| {
-        if let Ok(s) = std::str::from_utf8(data) {
-            let _ = toc_parser::parse(s);
-        }
-    });
+    inner::do_fuzz();
 }
