9de993f4383904816687a658b9b2f88923d7a615
DropDemBits <r3usrlnd@gmail.com>
2021-09-18 20:53:30 -0400

    Apply relative path movement transformations
    Reduces how many path normalizations need to be done

--- compiler/toc_vfs/src/vfs.rs
+++ compiler/toc_vfs/src/vfs.rs
@@ -9,6 +9,9 @@ use toc_span::FileId;
 use crate::intern::{PathInterner, PathResolution};
 use crate::BuiltinPrefix;
 
+// TODO: Unify path slashes to the platform preferred separator
+// This should be dependent on if we have legacy-mode path transformations enabled
+
 /// Concrete virtual filesystem interface
 ///
 // TODO: This behaviour has changed, and the `Vfs` is now just a path tree abstraction
@@ -44,10 +47,15 @@ impl Vfs {
             // Tack on the parent path
             let mut parent_path = self.path_interner.lookup_path(relative_to).to_owned();
             assert!(parent_path.pop(), "parent path for file was empty");
-            parent_path.join(expanded_path)
+
+            // Join paths together, applying path de-dotting
+            join_dedot(parent_path, &expanded_path)
         } else {
             // The only place that I can think that needs an absolute path is
             // for looking up the predef list, which always becomes an absolute path.
+            //
+            // However, even then the predef list is usually accessible via the `%oot`
+            // prefix, so it's likely just bad practice that's happening here
             //
             // For now, treat as-is, but may or may not be an absolute path.
             expanded_path
@@ -70,7 +78,7 @@ impl Vfs {
         let prefix_name = if_chain::if_chain! {
             if let Some(Component::Normal(comp)) = path.components().next();
             // If the given path component is not a valid unicode string, then it's safe to bail out
-            // None of the builtin percent prefixes contain any unicode characters.
+            // None of the builtin percent prefixes only contain alphabetic ascii characters.
             if let Some(comp) = comp.to_str();
             if let Some(prefix_name) = comp.strip_prefix('%');
             then {
@@ -87,7 +95,10 @@ impl Vfs {
                     .builtin_expansions
                     .get(&prefix_path)
                     .expect("missing path prefix");
-                base_path.join(path.strip_prefix(prefix_path.to_string()).unwrap())
+                join_dedot(
+                    base_path.to_owned(),
+                    path.strip_prefix(prefix_path.to_string()).unwrap(),
+                )
             }
             Err(_) => {
                 // No corresponding path prefix, return the path as-is
@@ -128,4 +139,57 @@ pub trait HasVfs {
 
     // Get mutable access to the underlying virtual file system
     fn get_vfs_mut(&mut self) -> &mut Vfs;
+}
+
+/// Joins two paths together, applying `ParentDir` and `CurrentDir` components
+///
+/// `append` must be a relative path
+fn join_dedot(mut path: PathBuf, append: &Path) -> PathBuf {
+    assert!(append.is_relative());
+
+    for comp in append.components() {
+        match comp {
+            Component::CurDir => {}
+            Component::ParentDir => {
+                // go up
+                path.pop();
+            }
+            Component::Normal(comp) => {
+                // append component
+                path.push(comp);
+            }
+            // absolute components
+            // should never be reachable since we check for absolute expanded paths
+            Component::Prefix(_) | Component::RootDir => unreachable!(),
+        }
+    }
+
+    path
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn resolve_dedots_expansion() {
+        let mut vfs = Vfs::new();
+
+        vfs.set_prefix_expansion(BuiltinPrefix::Oot, "/path/to/oot");
+        let help = vfs.intern_path("/path/to/some/help".into());
+
+        let resolve = vfs.resolve_path(None, "%oot/../other/.././oot/../some/help");
+        assert_eq!(resolve, PathResolution::Interned(help));
+    }
+
+    #[test]
+    fn resolve_dedots_relative() {
+        let mut vfs = Vfs::new();
+
+        let child = vfs.intern_path("/src/subdir/child".into());
+        let main = vfs.intern_path("/src/main.t".into());
+
+        let resolve = vfs.resolve_path(Some(child), "../././././main.t");
+        assert_eq!(resolve, PathResolution::Interned(main));
+    }
 }
