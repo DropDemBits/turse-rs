09b1353c32bf0096002b82e5cfe35e3b77780c6d
DropDemBits <r3usrlnd@gmail.com>
2023-01-01 23:36:43 -0500

    Check if `path_helper` can be removed

--- compiler/toc_vfs_db/src/query.rs
+++ compiler/toc_vfs_db/src/query.rs
@@ -23,7 +23,7 @@ where
         // Convert `path` into an absolute one
         let expanded_path = self.expand_path(path.into());
 
-        let full_path = if !path_helper::needs_joining(&expanded_path) {
+        let full_path = if expanded_path.is_absolute() {
             // Already an absolute path
             expanded_path
         } else if let Some(relative_to) = relative_to {
@@ -32,7 +32,7 @@ where
             assert!(parent_path.pop(), "parent path for file was empty");
 
             // Join paths together, applying path de-dotting
-            path_helper::join_dedot(parent_path, &expanded_path)
+            parent_path.join(expanded_path)
         } else {
             // The only place that I can think that needs an absolute path is
             // for looking up the predef list, which always becomes an absolute path.
@@ -68,10 +68,7 @@ where
             Ok(prefix_path) => {
                 let base_path = self.prefix_expansion(prefix_path);
 
-                path_helper::join_dedot(
-                    base_path,
-                    path.strip_prefix(prefix_path.to_string()).unwrap(),
-                )
+                base_path.join(path.strip_prefix(prefix_path.to_string()).unwrap())
             }
             Err(_) => {
                 // No corresponding path prefix, return the path as-is
@@ -140,106 +137,5 @@ where
         let result = (Arc::new(result.0), result.1);
 
         self.set_file_source(file_id, result);
-    }
-}
-
-/// ???: Do we need these (i.e. does camino handle it?)
-mod path_helper {
-    use camino::{Utf8Component, Utf8Path, Utf8PathBuf, Utf8Prefix};
-
-    /// Joins two paths together, applying `ParentDir` and `CurrentDir` components
-    ///
-    /// `append` must be a path that needs joining
-    pub(super) fn join_dedot(mut base_path: Utf8PathBuf, append: &Utf8Path) -> Utf8PathBuf {
-        assert!(needs_joining(append));
-
-        let mut comps = append.components().peekable();
-
-        // Deal with the first component
-        if let Some(first) = comps.peek() {
-            match *first {
-                Utf8Component::Prefix(prefix) => {
-                    if let Utf8Prefix::Disk(_) = prefix.kind() {
-                        assert!(!matches!(comps.next(), Some(Utf8Component::RootDir)));
-
-                        // Check if we need to fixup the drive prefix
-                        // Only need to do so if theres either a (different) drive prefix,
-                        // or no prefix at all
-
-                        let mut base_comps = base_path.components().peekable();
-
-                        let needs_fixup = match base_comps.peek() {
-                            // Do the fixup for drive prefix paths
-                            // Don't change drive for non-drive prefix paths
-                            Some(Utf8Component::Prefix(other_prefix)) => {
-                                matches!(other_prefix.kind(), Utf8Prefix::Disk(_))
-                            }
-                            // Append the drive prefix for all other components
-                            _ => true,
-                        };
-
-                        if needs_fixup {
-                            // Strip off existing drive prefix and join the rest of the path back
-                            let new_path = std::iter::once(Utf8Component::Prefix(prefix))
-                                .chain(
-                                    base_comps
-                                        .skip_while(|c| matches!(c, Utf8Component::Prefix(_))),
-                                )
-                                .collect();
-                            base_path = new_path;
-                        }
-                    } else {
-                        // per the assert above
-                        unreachable!()
-                    }
-                }
-                Utf8Component::RootDir => unreachable!("tried to append on a path with a root dir"),
-                _ => {}
-            }
-        }
-
-        for comp in comps {
-            match comp {
-                Utf8Component::CurDir => {}
-                Utf8Component::ParentDir => {
-                    // go up
-                    base_path.pop();
-                }
-                Utf8Component::Normal(comp) => {
-                    // append component
-                    base_path.push(comp);
-                }
-                // absolute component
-                // should never be reachable since we should've already dealt with them
-                Utf8Component::Prefix(_) | Utf8Component::RootDir => unreachable!(),
-            }
-        }
-
-        base_path
-    }
-
-    /// Returns true if the path is needs to be joined onto another path
-    pub(super) fn needs_joining(path: &Utf8Path) -> bool {
-        let mut comps = path.components();
-        let Some(first) = comps.next() else {
-        // empty path
-        return true;
-    };
-
-        match first {
-            Utf8Component::Prefix(prefix) => {
-                if matches!(prefix.kind(), Utf8Prefix::Disk(_)) {
-                    // Only consider for matching if the next component isn't a root dir
-                    !matches!(comps.next(), Some(Utf8Component::RootDir))
-                } else {
-                    // None of the other prefixes should be considered for joining
-                    false
-                }
-            }
-            // Doesn't need joining, driver directory will come from current drive
-            Utf8Component::RootDir => false,
-            // Definitely relative components
-            Utf8Component::CurDir | Utf8Component::ParentDir | Utf8Component::Normal(_) => true,
-        }
     }
 }
