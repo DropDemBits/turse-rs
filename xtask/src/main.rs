1989588fc995be8b04e94206ca54cc64bad068d7
DropDemBits <r3usrlnd@gmail.com>
2025-02-06 20:38:04 -0600

    Use miette for error reporting
    Needed for displaying pretty error results

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,15 +3,14 @@ mod flags;
 mod package;
 mod util;
 
+use miette::IntoDiagnostic;
 use util::project_root;
 use xshell::Shell;
 
-fn main() -> eyre::Result<()> {
-    color_eyre::install()?;
-
+fn main() -> miette::Result<()> {
     let flags = flags::Xtask::from_env_or_exit();
 
-    let sh = &Shell::new()?;
+    let sh = &Shell::new().into_diagnostic()?;
     sh.change_dir(project_root());
 
     match flags.subcommand {
